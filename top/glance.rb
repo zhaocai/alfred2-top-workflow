2f98c2e84da3a7493bf231674d165345866a2ab8
Zhao Cai <caizhaoff@gmail.com>
2013-10-02 22:58:27 -0400

    [major] glance battery and disk capacity

--- top/glance.rb
+++ top/glance.rb
@@ -6,10 +6,34 @@ require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
 
 require "bundle/bundler/setup"
 require "alfred"
+require 'plist'
+require 'awesome_print'
+require 'yaml'
 
 
+def percentage_sign(percent, use_sign = :emoji)
+  if use_sign.eql? :emoji
+    full = '🔴'
+    empty = '⚪'
+  elsif use_sign.eql? :plain
+    full = '●'
+    empty = '○'
+  elsif use_sign.eql? :fruit
+    signs = ["🍎", "🍎", "🍎", "🍊", "🍊", "🍊", "🍏" , "🍏", "🍏", "🍏", "🍏"]
+    mark = percent / 10
+    return signs[0...mark].join
+  end
+  mark = percent / 10
+  sign = ''
+  mark.times { |_| sign += full }
+  (10 - mark).times { |_| sign += empty }
+  sign
+end
 
-def generate_storage_feedback(feedback, query)
+def generate_storage_feedback(alfred, query)
+  return if query[1].eql? '⟩'
+
+  feedback = alfred.feedback
   devices = %x{/bin/df -H}.split("\n")
 
   devices.each do |device|
@@ -19,7 +43,7 @@ def generate_storage_feedback(feedback, query)
     size = items[1]
     used = items[2]
     free = items[3]
-    percentage = items[4]
+    percent = items[4][0...-1].to_i
     mount_point = items[8..-1].join(" ")
     if mount_point.eql? '/'
       name = 'Root'
@@ -28,17 +52,173 @@ def generate_storage_feedback(feedback, query)
     end
     feedback.add_file_item(mount_point,
                      :title => "#{name}: #{free} free",
-                     :subtitle =>"#{used} (#{percentage}) used of #{size} total")
+                     :subtitle =>"#{percent}%, #{used} used of #{size} total")
   end
 end
 
 
+def generate_bluetooth_battery_feedback(alfred, query)
+  return if query[1].eql? '⟩'
+
+  feedback = alfred.feedback
+
+  bluetooth_device_keys = ["BNBMouseDevice", "AppleBluetoothHIDKeyboard", "BNBTrackpadDevice"]
+
+  bluetooth_device_keys.each do |key|
+    devices = Plist.parse_xml %x{ioreg -l -n #{key} -r -a}
+    next if devices.nil? || devices.empty?
+
+    devices.each do |device|
+      name = device["Product"]
+      serial = device["SerialNumber"]
+      percent = device["BatteryPercent"].to_i
+      icon = {:type => "default", :name => "icon/bluetooth/#{key}.png"}
+      feedback.add_item(:subtitle => "#{percentage_sign(percent)} #{percent}%",
+                        :title => "#{name}",
+                        :uid => "#{key}: #{serial}",
+                        :icon => icon)
+    end
+  end
+
+end
+
+def generate_battery_feedback(alfred, query)
+
+  detailed_feedback = false
+  if query[1].eql? '⟩'
+    if query[0].eql? "Battery"
+      detailed_feedback = true
+    else
+      return
+    end
+  end
+
+
+  devices = Plist.parse_xml %x{ioreg -l -n AppleSmartBattery -r -a}
+  return if devices.nil? || devices.empty?
+  feedback = alfred.feedback
+
+  devices.each do |device|
+    current_capacity = device["CurrentCapacity"]
+    max_capacity     = device["MaxCapacity"]
+    design_capacity  = device['DesignCapacity']
+    temperature      = device['Temperature'].to_f / 100
+    charging         = device['IsCharging']
+    serial           = device['BatterySerialNumber']
+    cycle_count      = device['CycleCount']
+    fully_charged    = device['FullyCharged']
+    is_external      = device['ExternalConnected']
+    time_to_full     = device['AvgTimeToFull']
+    time_to_empty    = device['AvgTimeToEmpty']
+    manufacture_date = device['ManufactureDate']
+
+
+    day = manufacture_date & 31
+    month = (manufacture_date >> 5 ) & 15
+    year = 1980 + (manufacture_date >> 9)
+
+    manufacture_date = Date.new(year, month, day)
+    # month as unit
+    age = (Date.today - manufacture_date).to_f / 30
+
+    health           = max_capacity * 100 / design_capacity
+    percent           = current_capacity * 100 / max_capacity
+
+    status_info = 'Draining'
+    if charging
+      status_info = 'Charging'
+    elsif fully_charged
+      status_info = 'Fully Charged'
+    end
+
+    if percent > 80
+      icon_name = 'full'
+    elsif percent > 50
+      icon_name = 'medium'
+    elsif percent > 10
+      icon_name = 'low'
+    else
+      icon_name = 'critical'
+    end
+
+    time_info = 'Charging'
+
+    if charging
+      time_info = "#{time_to_full} until Full"
+    else
+      if fully_charged
+        if is_external
+          time_info = 'On AC Power'
+          icon_name = 'power'
+        else
+          time_info = "#{time_to_empty} Left"
+        end
+      end
+    end
+
+    icon = {:type => "default", :name => "icon/battery/#{icon_name}.png"}
+
+    battery_item = {
+      :title        => "Battery: #{status_info}"                 ,
+      :subtitle     => "#{percentage_sign(percent)} #{percent}%" ,
+      :uid          => "Battery: #{serial}"                      ,
+      :valid        => 'no'                                      ,
+      :autocomplete => 'Battery ⟩ '                              ,
+      :icon         => icon                                      ,
+    }
+
+    if detailed_feedback
+      battery_item[:valid] = 'yes'
+
+      feedback.add_item(battery_item)
+
+      feedback.add_item(
+        :title => "#{time_info}",
+        :subtitle => 'Time Left',
+        :icon => {:type => "default", :name => "icon/battery/clock.png"}
+      )
+      feedback.add_item(
+        :title => "#{temperature}° C",
+        :subtitle => 'Temperature',
+        :icon => {:type => "default", :name => "icon/battery/temp.png"}
+      )
+      feedback.add_item(
+        :title => "#{cycle_count}",
+        :subtitle => 'Charge Cycles Completed',
+        :icon => {:type => "default", :name => "icon/battery/cycles.png"}
+      )
+      feedback.add_item(
+        :title => "#{health}%",
+        :subtitle => 'Health',
+        :icon => {:type => "default", :name => "icon/battery/health.png"}
+      )
+      feedback.add_item(
+        :title => "#{serial}",
+        :subtitle => 'Serial Number',
+        :match? => :all_title_match?,
+        :icon => {:type => "default", :name => "icon/battery/serial.png"}
+      )
+      feedback.add_item(
+        :title => "#{age} monthes",
+        :subtitle => 'Age',
+        :icon => {:type => "default", :name => "icon/battery/age.png"}
+      )
+    else
+      feedback.add_item(battery_item)
+    end
+  end
+
+end
 
 def generate_feedback(alfred, query)
-  feedback = alfred.feedback
-  generate_storage_feedback(feedback, query)
+  generate_battery_feedback(alfred, query)
+  generate_bluetooth_battery_feedback(alfred, query)
+  generate_storage_feedback(alfred, query)
 
-  puts feedback.to_alfred(query)
+  if query[1].eql? '⟩'
+    query.shift 2
+  end
+  puts alfred.feedback.to_alfred(query)
 end
 
 
@@ -49,5 +229,4 @@ if __FILE__ == $PROGRAM_NAME
     generate_feedback(alfred, ARGV)
   end
 end
-
 
